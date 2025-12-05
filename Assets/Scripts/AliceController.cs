cbc0c4d0d2edff33755d5bfdb523a797c871a807
JustyceGoodeNU <goode.ju@northeastern.edu>
2025-12-05 15:02:56 -0500

    Fixed Alice AI to go through doors; created a red door prefab

--- Assets/Scripts/AliceController.cs
+++ Assets/Scripts/AliceController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class AliceController : MonoBehaviour
 {
-    //private Vector3 moveLocation;
     public GameObject player;
     public UnityEngine.AI.NavMeshAgent agent;
     public static bool followLucas = false;
@@ -10,22 +9,16 @@ public class AliceController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //moveLocation = null;
         //player = GameObject.Find("Lucas");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if(moveLocation != null)
-        // {
-        //     transform.position = moveLocation;
-        //     moveLocation = null;
-        // }
-
         if(followLucas)
         {
             agent.SetDestination(player.transform.position);
+            BaseballController.soundLocation = Vector3.zero;
         }
         else if(BaseballController.soundLocation != Vector3.zero)
         {
